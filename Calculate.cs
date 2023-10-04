0ffb8dc85db81db16e6a1a4651c4f1b6969f3054
min <mrblvck23@gmail.com>
2023-10-04 16:39:29 +0700

    bai tap mau

---
+++ Calculate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhanTheMinh_dubaothoitiethomnaymua
+{
+    class Calculate
+    {
+        private double a, b;
+
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public double B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public double Cong()
+        {
+            return a + b;
+        }
+        public double Tru()
+        {
+            return a - b;
+        }
+        public double Nhan()
+        {
+            return a * b;
+        }
+        public double Chia()
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhanTheMinh_dubaothoitiethomnaymua
+{
+    public partial class Form1 : Form
+    {
+        double a, b;
+        Calculate cal = new Calculate();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out a) == false)
+                errorProvider1.SetError(textBox1, "Please insert number");
+            else
+                errorProvider1.Clear();
+            cal.A = a;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox2.Text, out b) == false)
+                errorProvider2.SetError(textBox2, "Please insert number");
+            else
+                errorProvider2.Clear();
+            cal.B = b;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                textBox3.Text = Convert.ToString(cal.Cong());
+            }
+            else if (radioButton2.Checked)
+            {
+                textBox3.Text = Convert.ToString(cal.Tru());
+            }
+            else if (radioButton3.Checked)
+            {
+                textBox3.Text = Convert.ToString(cal.Nhan());
+            }
+            else
+            {
+                if (cal.B == 0)
+                {
+                    MessageBox.Show("Can not devide 0!", "Error!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                    textBox3.Text = Convert.ToString(cal.Chia());
+            }
+        }
+    }
+}
